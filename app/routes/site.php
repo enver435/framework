8fc3694b733fe2b1c36de4f25cc67b8770ef5fc5
enver435 <abbasovenver1999@gmail.com>
2018-03-03 23:58:43 +0400

    change language url

--- app/routes/site.php
+++ app/routes/site.php
@@ -3,7 +3,7 @@
 	# @Author: Enver Abbasov | https://www.facebook.com/enverabbasov435
 	# @Date:   2018-02-02 21:09:41
 	# @Email:  abbasovenver1999@gmail.com
-	# @Last Modified time: 2018-02-17 18:21:12
+	# @Last Modified time: 2018-03-03 23:58:29
 	
 	$route->get('/', function()
 	{
@@ -11,7 +11,7 @@
 	});
 
 	/* ROUTE LANGUAGE */
-	$route->get('/lang/{lang}?:isoCode2', function($lang)
+	$route->get('/{lang}?:isoCode2', function($lang)
 	{
 		if(file_exists(ROOT . 'app/language/' . $lang . '.php'))
 		{
